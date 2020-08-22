2b33307b1b32957f9a630ca3062f22eb8e766bdc
Ayo <aolag00@gmail.com>
2020-08-22 16:52:44 +0100

    Earth command retrieves, but not always an image for a particular date

--- src/commands/NASA/earth.js
+++ src/commands/NASA/earth.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const fetch = require('node-fetch');
 require('dotenv').config();
 const NASA_KEY = process.env.NASA_API_KEY;
 
@@ -9,10 +10,36 @@ module.exports = {
     usage: [' ', 'random', 'lat lon'],
     async execute(message, args) {
 
-        if (!args.length || args[0].toLowerCase() === 'random') {
-            const latitude = '';
-            const longitude = '';
+        // maximum possible longitude on the earth
+        const maxLongitude = 180;
+        // maximum possible latitude on the eartn
+        const maxLatitide = 85.05112878;
 
+        if (!args.length || args[0].toLowerCase() === 'random') {
+            // selects random latitude between 85.05112878 and - 85.05112878
+            const latitude = (Math.random() * (Math.floor(maxLatitide) - Math.ceil(-maxLatitide) + 1) + Math.ceil(-maxLatitide));
+            console.log(`lat=${latitude}`);
+            // selects a random longitude between 180 and -180
+            const longitude = (Math.random() * (Math.floor(maxLongitude) - Math.ceil(-maxLongitude) + 1) + Math.ceil(-maxLongitude));
+            console.log(`lon=${longitude}`);
+
+            const result = await fetch(`https://api.nasa.gov/planetary/earth/assets?lon=${longitude}&lat=${latitude}&api_key=${NASA_KEY}`);
+            const resultJson = await result.json().catch(error => console.log(error));
+
+            console.log(resultJson);
         }
     },
 };
+
+
+/**
+ * Creates an embed to display the image of the earth
+ * @param {json} result json object containing data returned from earth image request
+ */
+function showEarth(result) {
+
+    const earthEmbed = new Discord.MessageEmbed()
+        .setColor('#218f1f')
+        .setTitle('Random Earth Picture');
+
+}
